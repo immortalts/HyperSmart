ccc5eb7353bc41ae8ee9a2fcae73dc58e2b17d58
你好小羊 <2011543968@qq.com>
2024-08-06 17:30:11 +0800

    try release

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,22 @@
 import type { App } from 'vue'
 
+export const version: string
 export const install: (app: App) => void
 
 export * from './basicComponent'
 export * from './button'
 export * from './locale'
+export * from './styleVars'
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    HsBasicComponent: typeof import('@varlet/ui')['_BasicComponentComponent']
+    HsButton: typeof import('@varlet/ui')['_ButtonComponent']
+    HsLocale: typeof import('@varlet/ui')['_LocaleComponent']
+    HsStyleVars: typeof import('@varlet/ui')['_StyleVarsComponent']
+  }
+
+  export interface ComponentCustomProperties {
+    
+  }
+}
